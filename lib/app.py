1e2e3f34f3a3751af77a7ace7a840d41c38084fb
B-atuti <brian.atuti@student.moringaschool.com>
2023-08-16 13:00:48 +0300

    complete test

--- lib/app.py
+++ lib/app.py
@@ -1,11 +1,3 @@
+
 # lib/app.py
-print("Hello world!")
-print("Hello sun!")
-print("Hello sky!")
-
-# print ending
-
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! " )
-print("Hello sky!", end="!!!\n")
-
+print("Hello World! Pass this test, please.")
